44c5f90bbe1c0cc65961b7047c22a0eec40d3738
Mads Mikkel Rasmussen <mara@aspit.dk>
2023-02-27 09:37:27 +0100

    Made the note class

---
+++ Entities/Note.cs
@@ -0,0 +1,34 @@
+﻿namespace Entities
+{
+    public class Note
+    {
+        private int id;
+        private DateTime created;
+        private string title;
+        private string text;
+
+        public Note(int id, DateTime created, string title, string text)
+        {
+            Id = id;
+            Created = created;
+            Title = title;
+            Text = text;
+        }
+
+        public Note(DateTime created, string title, string text) 
+            :this(0, created, title, text)
+        {
+           // leave empty
+        }
+
+        public int Id { get => id; set => id = value; }
+        public DateTime Created { get => created; set => created = value; }
+        public string Title { get => title; set => title = value; }
+        public string Text { get => text; set => text = value; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
